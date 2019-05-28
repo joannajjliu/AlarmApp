330b72cc710a67a60c9d2444094fae2945e4a127
mllemango <msmangochan@gmail.com>
2019-05-28 17:58:04 -0400

    init commit

---
+++ app/src/main/java/com/example/hellopd/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.hellopd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import org.puredata.android.io.AudioParameters;
+import org.puredata.android.io.PdAudio;
+import org.puredata.android.utils.PdUiDispatcher;
+import org.puredata.core.PdBase;
+import org.puredata.core.utils.IoUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try{
+            initPD();
+            loadPDPatch();
+        }
+        catch (IOException e) {
+            Log.i("onCreate", "initialization and loading gone wrong :(");
+            finish();
+        }
+        initGUI();
+    }
+
+    private void initGUI(){
+        Switch onOffSwitch = (Switch) findViewById(R.id.onOffSwitch);
+        onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.i("onOffSwitch", String.valueOf(isChecked));
+                float val = (isChecked) ? 1.0f: 0.0f;
+                PdBase.sendFloat("onOff", val);
+            }
+        });
+    }
+
+    private void loadPDPatch(){
+        File dir = getFilesDir();
+        try {
+            IoUtils.extractZipResource(getResources().openRawResource(R.raw.simplepatch), dir, true);
+            Log.i("unzipping", dir.getAbsolutePath());
+        } catch (IOException e) {
+            Log.i("unzipping", "error unzipping");
+        }
+        File pdPatch = new File(dir, "simplepatch.pd");
+        try {
+            PdBase.openPatch(pdPatch.getAbsolutePath());
+        } catch (IOException e) {
+            Log.i("opening patch", "error opening patch");
+            Log.i("opening patch", e.toString());
+        }
+    }
+
+
+    private PdUiDispatcher dispatcher;
+
+    private void initPD() throws IOException {
+        int samplerate = AudioParameters.suggestSampleRate();
+        PdAudio.initAudio(samplerate, 0 , 2, 8, true);
+
+        dispatcher = new PdUiDispatcher();
+        PdBase.setReceiver(dispatcher);
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        PdAudio.startAudio(this);
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        PdAudio.stopAudio();
+    }
+}
